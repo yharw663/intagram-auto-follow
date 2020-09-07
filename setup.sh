6566b9bdc54855a603942fadd95450d738ad2939
yharw663 <70705589+yharw663@users.noreply.github.com>
2020-09-06 17:55:56 -0700

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+apt install git curl openssh openssl openssl-tool -y
+figlet -f big FINISHED !!! | lolcat
+xdg-open https://www.youtube.com/channel/UCiE0p7rXWBEncUVsLo1C5Xg
+echo
+echo -e "\e[1m Now Run bash instacrack.sh...!!!"
+echo
+echo -e "\e[1m\e[32m Developed by :\e[33m Tech Cochi"
